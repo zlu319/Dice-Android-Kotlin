b05a0e922ad852f02cfad68920a892a645a06285
Zhou Lu <msland2000@gmail.com>
2021-02-07 16:07:06 -0500

    Finished Dice with initialized values

--- test/Dice.kt
+++ test/Dice.kt
@@ -1,18 +1,36 @@
+/*
+@Author michael-land (zhou lu)
+Implementation of the following guide
+https://developer.android.com/codelabs/basic-android-kotlin-training-create-dice-roller-in-kotlin#5
+
+*/
+
+
 fun main() {
-    val myFirstDice = Dice()  //parentheses call constructor
+    val myFirstDice = Dice(6)  //parentheses call constructor
     //println(myFirstDice.sides)
     //val diceRoll = myFirstDice.roll()
-    println("Your ${myFirstDice.sides}-sided dice rolled ${myFirstDice.roll()}.")
+    println("Your ${myFirstDice.numSides}-sided dice rolled ${myFirstDice.roll()}.")
 
-    myFirstDice.sides = 20
-    println("Your ${myFirstDice.sides}-sided dice rolled ${myFirstDice.roll()}.")
+    //myFirstDice.numSides = 20 error, val cannot be reassigned (note you used val in the Dice class)
+    //println("Your ${myFirstDice.numSides}-sided dice rolled ${myFirstDice.roll()}.")
 
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides}-sided dice rolled ${mySecondDice.roll()}.")
 }
 
-class Dice {
+class DiceSix {
     var sides = 6  //variables are var; final (const) vals are val.
     fun roll(): Int {
         val randomNumber = (1..sides).random()
+        //println(randomNumber)
+        return randomNumber
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        val randomNumber = (1..numSides).random()
         //println(randomNumber)
         return randomNumber
     }
