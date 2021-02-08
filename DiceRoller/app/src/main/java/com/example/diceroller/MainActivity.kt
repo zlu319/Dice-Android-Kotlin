566f11fdae6b29e381ca5031514c062998a11fe6
Zhou Lu <msland2000@gmail.com>
2021-02-07 19:05:38 -0500

    Added a toast

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,25 @@
+/*
+@Author Zhou Lu
+ */
 package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        //R.id.button is the resource ID for the Button, which is a unique identifier for it.
+        // Android automatically assigns ID numbers to the resources in your app.
+        // Resource IDs are of the form R.<type>.<name>; for example, R.string.roll.
+        // For View IDs, the <type> is id, for example, R.id.button
+        rollButton.setOnClickListener({
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        })
     }
 }
